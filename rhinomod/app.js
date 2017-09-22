3f90ab57baa987fa055a44176e7fb2bdedf10bec
weimingtom <weimingtom@qq.com>
2017-09-23 06:52:38 +0800

    fib test

--- rhinomod/app.js
+++ rhinomod/app.js
@@ -1,3 +1,12 @@
-print('hello, world!');
-sleep(1000);
-toast('hello, toast!');
+//print('hello, world!');
+//sleep(1000);
+//toast('hello, toast!');
+
+function fib(n){
+    if(n==1||n==2){
+    	print(">>>END<<<");
+        return 1;
+    }
+    return fib(n-1)+fib(n-2);
+}
+print(fib(10));
